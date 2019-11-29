b0851b21e4bd816814da40ef0ed8be426a2d48c6
riccardo <riccardo94p@hotmail.com>
2019-11-29 16:39:55 +0100

    GET Client funzionanti

--- californium/ClientCoAP/src/ClientCoAP.java
+++ californium/ClientCoAP/src/ClientCoAP.java
@@ -17,7 +17,7 @@ public class ClientCoAP {
 		String addr = "";
 
 		URI uri = null;
-	
+		int node_id=0;
 
 		System.out.println("CLIENT STARTED");
 		System.out.println("Available commands:");
@@ -27,25 +27,21 @@ public class ClientCoAP {
 			System.out.print("\tInsert a command: ");
 			try {
 				rq = br.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			} catch (IOException e) { e.printStackTrace(); }
 
 			if(rq.equals("GET") || rq.equals("get")){
 				System.out.println("GET request for a node");
 				System.out.print("\tInsert now the node at witch you want to send the request: ");
 				try {
 					rq = br.readLine();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				} catch (IOException e) { e.printStackTrace(); }
 				
 				if(rq.matches("^[0-9]*$")){
 					System.out.println("GET request for node: " + rq);
 					try {
-						uri = new URI("coap://127.0.0.1:5683/TemperatureResource:"+ rq);
+						node_id = Integer.parseInt(rq);
+						node_id -=2;
+						uri = new URI("coap://127.0.0.1:5683/TemperatureResource:"+ node_id);
 					} catch (URISyntaxException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -55,13 +51,16 @@ public class ClientCoAP {
 					CoapResponse response = client.get();
 					if (response!=null) {
 						
-						System.out.println(response.getCode());
-						System.out.println(response.getOptions());
-						System.out.println(response.getResponseText());
+						//System.out.println(response.getCode());
+						//System.out.println(response.getOptions());
+						System.out.print("\033[H\033[2J");  //"clear" the screen
+	    					System.out.flush(); 
+						System.out.println("Temperature at node "+ (node_id+2) +": "+response.getResponseText()+"°C");
 						
-						System.out.println("\nADVANCED\n");
+						
 						// access advanced API with access to more details through .advanced()
-						System.out.println(Utils.prettyPrint(response));
+						//System.out.println("\nADVANCED\n");
+						//System.out.println(Utils.prettyPrint(response));
 						
 					} else {
 						System.out.println("No response received.");
